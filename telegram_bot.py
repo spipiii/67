e35c7f3ea9b90d636de3be7949707b37cd75d6a1
spipiii <spipizxc@gmail.com>
2026-03-14 18:50:55 +0200

    Remove parse_mode from send_message function

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,7 @@
 from telegram import Bot
+
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
@@ -8,6 +10,5 @@ async def send_message(text):
 
     await bot.send_message(
         chat_id=TELEGRAM_CHAT_ID,
-        text=text,
-        parse_mode="HTML"
+        text=text
     )
